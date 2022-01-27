67392c85ae68d0991938b817d46670f8d2a35125
Swiftb0y <12380386+Swiftb0y@users.noreply.github.com>
2022-01-27 21:15:55 +0100

    add convert_all script

---
+++ convert_audio_to_flac/README.md
@@ -0,0 +1,3 @@
+# Convert All
+
+takes a single input, which is the file extension. It then encodes all the files matching that extension in the current directory to flac using ffmpeg.
---
+++ convert_audio_to_flac/convert_all.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+
+if [ $# -lt "1" ]; then
+        echo "Usage:  $0 <format>"
+	echo "format: audio extension of the input files"
+        exit 1;
+fi
+
+#https://stackoverflow.com/questions/5784661/how-do-you-convert-an-entire-directory-with-ffmpeg
+
+format=$1
+
+for i in *.$format; do
+  name=${i%.*}
+  echo "$name"
+  (ffmpeg -hide_banner -i "$i" "${name}.flac" && mv "$i" /tmp/) &
+done
+
+wait
