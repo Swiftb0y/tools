e936a92e5c1dd4005dcd26c2d44fb4c484c195de
Swiftb0y <12380386+Swiftb0y@users.noreply.github.com>
2022-03-10 00:37:52 +0100

    feat(convert_audio_to_flac): arg handing and feats

--- convert_audio_to_flac/convert_all.sh
+++ convert_audio_to_flac/convert_all.sh
@@ -1,20 +1,54 @@
 #!/usr/bin/env bash
 
+# enable recursive glob pattern (**/*)
+shopt -s globstar
 
-if [ $# -lt "1" ]; then
-        echo "Usage:  $0 <format>"
-	echo "format: audio extension of the input files"
+usage() {
+        echo "Usage: $0 [-r] [-i in_format] [-o out_format] [dir]"
+	echo "r: no recurse directories"
+	echo "i: audio extension of the input files"
+	echo "o: audio extension of the output files"
+        echo "dir: working directory from where to start matching"
         exit 1;
-fi
+}
+
+SCAN_DIR="$(pwd)"
+RECURSE="**/"
+I_FORMAT="wav"
+O_FORMAT="flac"
+
+while getopts "i:o:r" opts; do
+    case "${opts}" in
+        r)
+            RECURSE=""
+            ;;
+        i)
+            I_FORMAT=${OPTARG}
+            ;;
+        o) 
+            O_FORMAT=${OPTARG}
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND -1))
+
+# get last arg after positional args
+if [[ -n $1 ]]; then SCAN_DIR="$1"; fi
+# normalize path
+SCAN_DIR="$(realpath "${SCAN_DIR}")/"
+
+# build scan path glob for iteration
+GLOB="${SCAN_DIR}${RECURSE}*.${I_FORMAT}"
 
 #https://stackoverflow.com/questions/5784661/how-do-you-convert-an-entire-directory-with-ffmpeg
 
-format=$1
-
-for i in *."$format"; do
+for i in $GLOB; do
   name=${i%.*}
   echo "$name"
-  (ffmpeg -hide_banner -i "$i" "${name}.flac" && mv "$i" /tmp/) &
+  (ffmpeg -hide_banner -i "$i" "${name}.${O_FORMAT}" && mv "$i" /tmp/) > /dev/null & 
 done
 
 wait
